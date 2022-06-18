5101f7932eacebd64ff54545ca2ddf3a6280163d
Atharv Bhasme <94709794+CodeHub5199@users.noreply.github.com>
2022-06-18 19:48:50 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pickle
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import  PorterStemmer
+import string
+import requests
+import json
+import plotly.graph_objects as go
+
+ps = PorterStemmer()
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    text = y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+    return ' '.join(y)
+tfidf = pickle.load(open('vectorizer_tfidf.pkl','rb'))
+model = pickle.load(open('model_log.pkl','rb'))
+
+r = requests.get('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7c053139fa444fd1b03371509b60b9b2')
+# r = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=7c053139fa444fd1b03371509b60b9b2')
+
+data = json.loads(r.content)
+positive = []
+negative = []
+for z in range(20):
+    news = data['articles'][z]['title']
+    news_input = news
+    transformed_msg = transform_text(news_input)
+    vector_input = tfidf.transform([transformed_msg])
+    result = model.predict(vector_input)[0]
+    print(z+1,'. ',news)
+    if result == 0:
+        print('Predicted Sentiment: Negative')
+        negative.append(result)
+    else:
+        print('Predicted Sentiment: Positive')
+        positive.append(result)
+print('Length of positive',len(positive))
+sentiment_score = 20-len(positive)
+percentage = (100*sentiment_score)/20
+print('Percentage: ',percentage)
+fig = go.Figure(go.Indicator(
+    mode = "gauge+number",
+    number = {'suffix': "%"},
+    value = percentage,
+    gauge={
+        'axis':{'range':[0,100]},
+        'bar':{'color':'lightsteelblue'},
+        'steps':[
+            {'range':[0,45],'color':'tomato'},
+            {'range':[55,100],'color':'lime'}
+        ]},
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    title = {'text': "Sentiment Indicator"}))
+# fig.show()
